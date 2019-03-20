d6a589abc34e5625a0dcc0e39c789831a34b237f
nboutin <boutwork@gmail.com>
2019-03-20 11:47:58 +0100

    add cmake_external

---
+++ cmake_external/src/main.cpp
@@ -0,0 +1,15 @@
+//! \file	main.cpp
+//! \date	Jul 3, 2018
+//! \author	OS Services
+//! \copyright	(C) Copyright Magneti Marelli 2018
+//!
+//! Project: TBM2 NAD\n
+//! Software Component: Component Name
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
